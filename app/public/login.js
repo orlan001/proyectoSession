a5861581f3e40f525c180d652b38ac7b399cb7a3
orlan001 <orlandoma68@gmail.com>
2024-05-31 20:38:39 -0300

    modificado-api-login

--- app/public/login.js
+++ app/public/login.js
@@ -1,13 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
 
 const error = document.querySelectorAll('.error')[0];
 document.getElementById("login-form").addEventListener('submit', async (e)=>{
     e.preventDefault();
     const user = e.target.children.user.value;
     const password = e.target.children.password.value;
-    const url = "https://proyectosession-2.onrender.com/:"+process.env.PORT
-    const res = await fetch(url+"/api/login",{
+    const res = await fetch(`/api/login`,{
         method:"POST",
         headers:{
             "Content-Type": "application/json",
@@ -16,7 +13,7 @@ document.getElementById("login-form").addEventListener('submit', async (e)=>{
             user, password
         })
     });
-  
+   console.log(res);
     if(!res.ok) return error.classList.toggle('escondido', false);
     const resJson = await res.json();
     if(resJson.redirect){
